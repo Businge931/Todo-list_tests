302ce0ed99951240ab55249de40e35bc108c1de4
Businge931 <jimmybusinge9@gmail.com>
2023-08-11 22:56:54 +0300

    test: added tests for no. of incomplete todos

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,32 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import TodoFooter from "./../TodoFooter";
+import { BrowserRouter } from "react-router-dom";
+
+const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  );
+};
+
+describe("TodoFooter", () => {
+  test("should render the correct number of incomplete tasks", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test("should render 'task' when number of numberOfIncompleteTasks is 1", async () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeTruthy();
+  });
+});
+
+// test("should render numberOfIncompleteTasks to the user", async () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toBeVisible();
+// });
