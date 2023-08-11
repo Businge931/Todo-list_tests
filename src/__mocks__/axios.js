2b7b71ff198e3d49db5c869b3f09670707e74a6d
Businge931 <jimmybusinge9@gmail.com>
2023-08-12 00:36:29 +0300

    test:mocking the api call with dummy data

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,23 @@
+const mockResponse = {
+  data: {
+    results: [
+      {
+        name: {
+          first: "Laith",
+          last: "Harb",
+        },
+        picture: {
+          large: "https://randomuser.me/api/portraits/men/59.jpg",
+        },
+        login: {
+          username: "ThePhonyGOAT",
+        },
+      },
+    ],
+  },
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  get: jest.fn().mockResolvedValue(mockResponse),
+};
